324588b1f7d6302296d773263527776272516391
Kypulse <munro2767@gmail.com>
2025-07-30 16:35:24 +0100

    Update Main.java

--- src/main/java/skid/krypton/Main.java
+++ src/main/java/skid/krypton/Main.java
@@ -1,9 +1,18 @@
 package skid.krypton;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 
-public final class Main implements ModInitializer {
-    public void onInitialize() {
+public final class Main implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
         new Krypton();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.getWindow() != null) {
+                client.getWindow().setTitle("Krypton On Crack│https://discord.gg/g3h87TyGs6");
+            }
+        });
     }
 }
